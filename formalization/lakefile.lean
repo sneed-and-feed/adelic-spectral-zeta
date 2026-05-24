793c7faf1c3d5f33794e40971aff409bcbe6384b
Cthulhu Skates <cthulhu.skates@gmail.com>
2026-05-24 12:02:55 -0700

    Fix lakefile.lean parser error by adding open Lake DSL

--- formalization/lakefile.lean
+++ formalization/lakefile.lean
@@ -1,11 +1,10 @@
 import Lake
-open Lake
+open Lake DSL
 
 package «formalization» where
   -- Settings applied to both builds and targets
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.8.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.8.0"
 
 @[default_target]
 lean_lib «Formalization» where
