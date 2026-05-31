e1223ffe9a0fae284f6b83bbf1aa008f19d7842a
Cthulhu Skates <cthulhu.skates@gmail.com>
2026-05-30 20:43:13 -0700

    fix: remove Trainer from top-level init to avoid broken torchvision crash on Colab

--- src/llama_surgery/__init__.py
+++ src/llama_surgery/__init__.py
@@ -1,5 +1,4 @@
 from .llama_patcher import inject_surgery
 from .surgery import SurgicalLlamaAttention
-from .surgery_trainer import SurgeryTrainer
 
-__all__ = ["inject_surgery", "SurgicalLlamaAttention", "SurgeryTrainer"]
+__all__ = ["inject_surgery", "SurgicalLlamaAttention"]
