a0b14a3be685547a20fd30ea0c9c3f5521b34b93
Cthulhu Skates <cthulhu.skates@gmail.com>
2026-05-31 01:12:39 -0700

    Package llama_surgery as a pip-installable library with clean exports and optimized build inclusion

--- src/llama_surgery/__init__.py
+++ src/llama_surgery/__init__.py
@@ -1,4 +1,13 @@
 from .llama_patcher import inject_surgery
-from .surgery import SurgicalLlamaAttention
+from .surgery import SurgicalLlamaAttention, SurgeryLossRamp
+from .surgery_trainer import SurgeryTrainer, TauAnnealingCallback
+from .topology import DynamicTopologyRouter
 
-__all__ = ["inject_surgery", "SurgicalLlamaAttention"]
+__all__ = [
+    "inject_surgery",
+    "SurgicalLlamaAttention",
+    "SurgeryLossRamp",
+    "SurgeryTrainer",
+    "TauAnnealingCallback",
+    "DynamicTopologyRouter",
+]
